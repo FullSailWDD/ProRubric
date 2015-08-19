cbbf8c478ee2e1fbeb9d91523bd555285d6d3b1a
Chapman <chapman@apextion.com>
2015-08-19 12:06:41 -0400

    Add outputs.debug() ability / replacement for console log

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // npm modules
 var express 				= require('express'),
 		app 						= express(),
-  	exphbs 					= require('express-handlebars');
+  	exphbs 					= require('express-handlebars'),
+  	outputs 				= require('./lib/outputs.js')();
 
 
 // View Config
@@ -32,4 +33,4 @@ app.use(express.static(__dirname +'/public'));
 // START THE SERVER
 // =============================================================================
 var server 					= app.listen(port);
-console.log('Starting Node Server on Port ' + port);
+outputs.debug(port, "Node Server Port Status", true);
