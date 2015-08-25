9be51fcbcd39ed11dfb94e7449f8ca0ce9509ac9
Patrick Halton <phalton04@fullsail.edu>
2015-08-24 22:07:00 -0400

    merge correction in master.js

--- routes/master.js
+++ routes/master.js
@@ -6,23 +6,22 @@ module.exports = function(app, socket) {
         Section = require('../models/section.js'),
         LineItem = require('../models/lineItem.js');
 
-
     // route /
     app.get('/', function(req, res) {
-//        socket.on('connection', function (data) {
-//                Degree.model.find({}, function (err, doc) {
-//                    if (err) {fail(err)}else{
-//                        var array=[];
-//                        for(i=0; i<doc.length;i++){
-//                            array.push(doc[i]);
-//                        }
-//                        socket.emit('find degrees',array);
-//                    }
-//                });
-//            data.on('add degree', function (callback) {
-//                Degree.add(callback);
-//            });
-//        });
+        socket.on('connection', function (data) {
+                Degree.model.find({}, function (err, doc) {
+                    if (err) {fail(err);}else{
+                        var array=[];
+                        for(i=0; i<doc.length;i++){
+                            array.push(doc[i]);
+                        }
+                        socket.emit('find degrees',array);
+                    }
+                });
+            data.on('add degree', function (callback) {
+                Degree.add(callback);
+            });
+        });
         res.render('index');
     });
 
